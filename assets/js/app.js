d9da6c97e3e9cf61e27f45316f135f6d38eea05f
Masna Bin Umeed <masnabinumeed@gmail.com>
2020-06-02 02:58:36 +0500

    no message

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,3 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-96671273-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-96671273-2');
-</script>
-
-
-
 
 
 window.onload = function(){
