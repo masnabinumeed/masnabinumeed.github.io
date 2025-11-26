1b97021757c3e0f206886d143580ef73669c22ad
Masna Bin Umeed <masnabinumeed@gmail.com>
2025-11-27 01:57:08 +0500

    About me section and relve project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,58 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  // Modal functionality
+  var modal = document.querySelector('.modal');
+  var btn = document.querySelector('.button');
+  var span = document.querySelector('.close');
 
-
-window.onload = function(){
-
-  // Get the modal
-  var modal = document.getElementsByClassName("modal")[0];
-
-  // Get the button that opens the modal
-  var btn = document.getElementsByClassName("button")[0];
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (btn && modal) {
+    btn.addEventListener('click', function() {
+      modal.style.display = 'block';
+    });
   }
-}
-}
 
-
-// Scroll function courtesy of Scott Dowding; http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
-
-$(document).ready(function() {
-  // Check if element is scrolled into view
-  function isScrolledIntoView(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-
-    return elemBottom <= docViewBottom && elemTop >= docViewTop;
+  if (span && modal) {
+    span.addEventListener('click', function() {
+      modal.style.display = 'none';
+    });
   }
-  // If element is scrolled into view, fade it in
-  $(window).scroll(function() {
-    $(".lazyscroll").each(function() {
-      if (isScrolledIntoView(this) === true) {
-        $(this).addClass("fadein");
+
+  if (modal) {
+    window.addEventListener('click', function(event) {
+      if (event.target === modal) {
+        modal.style.display = 'none';
       }
     });
-  });
+  }
 
+  // Check if element is scrolled into view (triggers when 25% visible)
+  function isScrolledIntoView(elem) {
+    var rect = elem.getBoundingClientRect();
+    var windowHeight = window.innerHeight;
+    var triggerPoint = rect.top + (rect.height * 0.25);
+    
+    return triggerPoint <= windowHeight && rect.bottom >= 0;
+  }
 
-  $( window ).on( "load", function() {
-    $(".onload").addClass("fadein");
+  // Lazyscroll - fade in elements on scroll
+  function checkLazyScroll() {
+    var elements = document.querySelectorAll('.lazyscroll');
+    elements.forEach(function(elem) {
+      if (isScrolledIntoView(elem)) {
+        elem.classList.add('fadein');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', checkLazyScroll);
+  checkLazyScroll(); // Check on initial load
+
+  // Onload - fade in elements when page loads
+  window.addEventListener('load', function() {
+    var elements = document.querySelectorAll('.onload');
+    elements.forEach(function(elem) {
+      elem.classList.add('fadein');
+    });
   });
 
 });
